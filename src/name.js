6af286e7b7ab876a10273aa89f3856db629de17d
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-01-09 13:15:46 +0530

    changes

---
+++ src/name.js
@@ -0,0 +1,11 @@
+const axios = require("axios").default;
+
+function Name(count = 1, data) {
+  axios
+    .get("https://random-data-api.com/api/name/random_name?size=" + count)
+    .then((res) => {
+      data(res.data);
+    });
+}
+
+module.exports = Name;
